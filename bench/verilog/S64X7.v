526c479bb9a5d644d335d768ac09802d321a5cd7
Samuel A. Falvo II <sam.falvo@rackspace.com>
2016-08-12 12:57:51 -0700

    Refactor

--- bench/verilog/S64X7.v
+++ bench/verilog/S64X7.v
@@ -1,25 +1,6 @@
 `timescale 1ns / 1ps
 
-`define OPC_NOP     4'b0000
-`define OPC_LIT8    4'b0001
-`define OPC_LIT16   4'b0010
-`define OPC_LIT32   4'b0011
-`define OPC_STORES  4'b0100
-`define OPC_LOADS   4'b0101
-
-`define N_SBM       4'b0000
-`define N_SHM       4'b0001
-`define N_SWM       4'b0010
-`define N_SDM       4'b0011
-
-`define N_LBMU      4'b0000
-`define N_LHMU      4'b0001
-`define N_LWMU      4'b0010
-`define N_LDMU      4'b0011
-`define N_LBMS      4'b0100
-`define N_LHMS      4'b0101
-`define N_LWMS      4'b0110
-`define N_LDMS      4'b0111
+`include "opcodes.vh"
 
 module test_S64X7();
   reg [23:0]  story_o;
